920ac5fd9bf6f4ecfa5f9e4d3489632b98745b0d
Lutendo Damuleli <lutendo.damuleli@outlook.com>
2024-01-10 14:26:04 +0200

    init files

---
+++ src/main/java/com/enviro/assessment/grad001/lutendodamuleli/LutendodamuleliApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.lutendodamuleli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutendodamuleliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LutendodamuleliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/lutendodamuleli/LutendodamuleliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.lutendodamuleli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LutendodamuleliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
